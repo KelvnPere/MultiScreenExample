be8b1e03492eca6644b8a31aa5300537561c46ed
Pheonix73 <codebear73@gmail.com>
2020-06-13 22:53:47 +0100

    Initial commit

---
+++ app/src/main/java/com/fashi/multiscreenexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.fashi.multiscreenexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO
+    // Create the other activity for the other images to display the tour
+    // Change the Next Button to use a arrow image
+    // Add Functonality to the app to move to next activity
+    // Make the app a little bit fine.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/fashi/multiscreenexample/Tour1Activity.java
@@ -0,0 +1,14 @@
+package com.fashi.multiscreenexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tour1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tours);
+    }
+}
